6d37bc2b3fbfe6d29e96a0693d3aba797a6b3f3c
Yoav Beiser <yoavbeiser0227@gmail.com>
2023-10-22 18:29:33 +0300

    add class Person

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 
+class Person:
+    number_of_people = 0
 
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+
+    def __str__(self):
+        return f"name is {self.name} and {self.age} years old"
+    
 def greeting():
     name = input("what is your name?: ")
     print(f"Hello {name}")
