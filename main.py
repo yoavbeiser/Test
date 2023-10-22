93f627c660d459435c8c6c2344a3004b00b3bab6
Yoav Beiser <yoavbeiser0227@gmail.com>
2023-10-22 18:40:17 +0300

    reformat + add code for checking class

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 class Person:
     number_of_people = 0
 
@@ -12,12 +11,20 @@ class Person:
     def __eq__(self, other):
         print("equals method called")
         return self.age == other.age and self.name == other.name
+
+
 def greeting():
     name = input("what is your name?: ")
     print(f"Hello {name}")
+
+
 def run():
-    print("welcome...")
-    greeting()
+    # print("welcome...")
+    # greeting()
+    p1_yoav = Person(18, "Yoav")
+    p2_roy = Person(18, "Roy")
+    print(f"p1_yoav.__eq__(p2_roy): {p1_yoav.__eq__(p2_roy)}")
+
 
 if __name__ == "__main__":
     run()
