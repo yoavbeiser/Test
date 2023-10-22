d057d059e58c8709f5ac6eef3a2b9f7a9bef7595
Yoav Beiser <yoavbeiser0227@gmail.com>
2023-10-22 18:22:50 +0300

    add run and greeting functions

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+
+
+def greeting():
+    name = input("what is your name?: ")
+    print(f"Hello {name}")
+def run():
+    print("welcome...")
+    greeting()
+
+if __name__ == "__main__":
+    run()
